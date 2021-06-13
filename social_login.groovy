d30417b1de4c914afdbb61afdf331064d299f6d5
manikandanchellam <60849723+manikandanchellam@users.noreply.github.com>
2021-06-13 10:01:21 +0530

    midified

--- social_login.groovy
+++ social_login.groovy
@@ -1,82 +1,83 @@
 #!/groovy
+
 pipeline {
-  agent any
-  parameters {
-    gitParameter(branch: '', branchFilter: '.*', defaultValue: 'master',
-      description: 'enter the branch name',
-      name: 'branch', quickFilterEnabled: false,
-      selectedValue: 'NONE', sortMode: 'NONE',
-      tagFilter: '*', type: 'PT_BRANCH')
-
-    string(name: 'PERSON', defaultValue: 'Mr Manikandan', description: 'Running from my local branch')
-
-    choice(name: 'CHOICE', choices: ['DEV', 'QA', 'UAT'], description: 'Pick QA')
-
-    booleanParam(name: 'BUILD', defaultValue: false, description: '')
-  }
-  tools {
-    jdk 'java_home'
-    maven 'maven_home'
-  }
-  stages {
-    stage('checkout') {
-      steps {
-        echo "checkout"
-        checkout([$class: 'GitSCM',
-          branches: [
-            [name: "${params.branch}"]
-          ],
-          extensions: [],
-          userRemoteConfigs: [
-            [url: 'https://github.com/manikandanchellam/webapplication.git']
-          ]
-        ])
-      }
+    agent any
+    //triggers {
+      // cron(*/1 * * * *)
+    //}
+    parameters{
+       string(name: 'PERSON', defaultValue: 'Abinaya', description: 'Running from my local branch') 
+       choice(name: 'Env', choices: ['Dev', 'QA', 'PROD'], description: 'choose your environment')
+       gitParameter(branch: '', branchFilter: '.*', defaultValue: 'master', description: 'enter the branch name', name: 'branch', quickFilterEnabled: false, selectedValue: 'NONE', sortMode: 'NONE', tagFilter: '*', type: 'PT_BRANCH')
+       booleanParam(name: 'BUILD', defaultValue: true, description: 'Build the code')
     }
-    stage('Unit test') {
-      steps {
-        echo "Unit test skipped"
-        echo "HELLO ${params.PERSON}"
-      }
+    tools{
+        jdk 'java_home'
+        maven 'maven_home'
     }
-    stage('Code quality') {
-      steps {
-        echo "Code quality skipped"
-        echo "Choice: ${params.CHOICE}"
-      }
-    }
-    stage('Publish reports') {
-      steps {
-        echo "Publish reports skipped"
-      }
-    }
-    stage('Build') {
-      when{
-        expression {return params.BUILD}
-      }
-      steps {
-        echo "Build  ${params.BUILD}"
-        sh 'mvn clean install'
-      }
-    }
-    stage('publish to Artifcate') {
-      steps {
-        echo "publishing"
-      }
-    }
-    stage('build docker') {
-      steps {
-        echo "create doceker image"
-      }
-    }
-      stage('deploy') 
-        {
-        when {
-         expression { params.BUILD == true }
-                }
-         steps {
-                 sh 'cp target/java-tomcat-maven-example.war  /var/lib/tomcat/webapps/'
-                }
+    stages{
+        stage('checkout'){
+            steps{
+                echo "checkout"
+                checkout([$class: 'GitSCM', branches: [[name: "${params.branch}"]],
+                extensions: [], 
+                userRemoteConfigs: [[url: 'https://github.com/AbinayaRavikannan/webapplication.git']]])
+        }
+        }
+        stage('test case'){
+            steps{
+                echo "test case skipping"
+                echo "HELLO ${params.PERSON}"
+        }
+        }
+        stage('code quality'){
+            steps{
+                echo "test case skipping"
+        }
+        }
+        stage('publish report'){
+            steps{
+                echo "publish report skipping"
+        }
+        }
+        stage('build'){
+            when {
+                  expression { return params.BUILD }
+            }
+            steps{
+                    echo "build  ${params.BUILD}"
+                    sh 'mvn clean install'
+        }
+        }
+        stage('Publish to artifact'){
+            steps{
+                echo "Publish to artifact"
+        }
+        }
+        stage('Build docker image'){
+            steps{
+                echo "Build docker image"
+        }
+        }
+        stage('deploy'){
+            steps{
+                echo "deploy"
+                sh 'cp target/java-tomcat-maven-example.war /var/lib/tomcat/webapps' 
+        }
+        }
+    } 
+    post {
+        always {
+            cleanWs() /* clean up our workspace */
+        }
+        success {
+            echo 'I succeeded!'
+        }
+        failure {
+            echo 'I failed :('
+        }
+        changed {
+            echo 'Things were different before...'
         }
     }
 }
